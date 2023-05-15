95ff146856e1d5abff1a00bdf77aa46882eedc73
Hugo DESCHAMPS <hpvdeschamps@gmail.com>
2023-05-15 14:33:32 +0200

    Ajout de nouvelles textures et changement du mode gfx de fullscreen à windowed 800/600

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ void initialiation_allegro(){
     set_color_depth(desktop_color_depth()); //ici�: identique � celle du bureau
     //s�lection du mode graphique
     // avec choix d'un driver+mode+r�solution de l'�cran
-    if (set_gfx_mode(GFX_AUTODETECT_FULLSCREEN,1920,1200,0,0)!=0)
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
     {
         // message et sortie de prog. si �chec
         allegro_message("prb gfx mode");
@@ -23,34 +23,7 @@ void initialiation_allegro(){
     }
 
 }
-BITMAP* buffer;
-BITMAP* tete,*queue,*fond;
-void snake(){
-    buffer=create_bitmap(SCREEN_W,SCREEN_H);
-    clear_bitmap(buffer);
-
-    // Chargement de l'image
-    tete=load_bitmap("tete_s.bmp",NULL);
-    if (!tete)  {
-        allegro_message("pas pu trouver/charger tete_s.bmp");
-        allegro_exit(); exit(EXIT_FAILURE);
-    }
-    queue=load_bitmap("queue_s.bmp",NULL);
-    if (!queue)  {
-        allegro_message("pas pu trouver/charger queue_s.bmp");
-        allegro_exit(); exit(EXIT_FAILURE);
-
-    }fond=load_bitmap("fond_s.bmp",NULL);
-    if (!fond)  {
-        allegro_message("pas pu trouver/charger fond_s.bmp");
-        allegro_exit(); exit(EXIT_FAILURE);
-    }
-
-
-    blit(fond,screen,0,0,0,0,SCREEN_W,SCREEN_H);
-    while (!key[KEY_ESC]){
-    }
-}
+BITMAP *buffer;
 
 int menu(){
     clear_bitmap(buffer);
